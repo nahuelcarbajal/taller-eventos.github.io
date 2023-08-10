6b3f16af3aa079cb5c20a3d63e248285796d3faa
Nahuel-sudo <141088503+Nahuel-sudo@users.noreply.github.com>
2023-08-09 23:08:50 -0300

    Solucion-Problema

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 document.addEventListener("DOMContentLoaded", () =>{
 
     let alerta = document.querySelector("div");
+    let btn = document.querySelector("button");
 
         alerta.addEventListener("click", () =>{
         alert("¡Hola! Soy el div");
+        });
+
+        btn.addEventListener("click", e=>{
+            event.stopPropagation();
+            alert("Hola!");
         });
 });
